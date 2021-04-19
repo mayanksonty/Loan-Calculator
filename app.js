28af93e88de119f61790736129545d4121502759
mayanksonty <mayanksonty@gmail.com>
2021-04-19 14:00:57 +0530

    Modified JavaScript File and added the Functionality

---
+++ app.js
@@ -0,0 +1,61 @@
+// Listening the submit button
+
+document.querySelector('#loan-form').addEventListener('submit',calculateResults);
+
+function calculateResults(e) {
+
+    const amount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthlyPayment = document.querySelector('#monthly-payment');
+    const totalPayment = document.querySelector('#total-payment');
+    const totalInterest = document.querySelector('#total-interest');
+  
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+  
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+  
+    if(isFinite(monthly)) {
+      monthlyPayment.value = monthly.toFixed(2);
+      totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+      totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    } else {
+      showError('Please check your numbers');
+    }
+
+    e.preventDefault();
+}
+
+// show error
+
+function showError(error){
+    // create a Div
+    const errorDiv = document.createElement('div');
+
+    // Get Element
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add Class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert Error Above Heading
+    card.insertBefore(errorDiv,heading)
+
+    // set Time out
+    setTimeout(clearError,2000)
+
+}
+
+function clearError(){
+    document.querySelector('.alert').remove();
+  }
+
+  
